97426bb87df2c125f0d265ed96c496d1a31cf55a
Erneststomp <ernesto.lopez.lbm@outlook.com>
2022-06-22 22:56:50 -0500

    implementacion de bootstrap 5

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,17 +6,27 @@ import CartWidget from "../CartWidget/CartWidget";
  const NavBar = () => { 
     return(
     <div className="topnav" id="myTopnav">
-        <div className="NavBar__Container">
-            <img className="logo__size" alt="" src={Logo}/>    
-            <ul className="NavBar__Align">
-                <li className="NavBar__Fromat"><a className="" href="index.html">Inicio</a></li>
-                <li className="NavBar__Fromat"><a className="" href="about.html">Nosotros</a></li>
-                <li className="NavBar__Fromat"><a className="" href="clients.html">Nuestros Clientes</a></li>
-                <li className="NavBar__Fromat"><a className="" href="service.html">Tienda</a></li>
-                <li className="NavBar__Fromat"><a className="" href="cart.html"><CartWidget/></a></li>
-            </ul>
-                 
-        </div>        
+        <nav class="navbar navbar-expand-sm navbar-dark  gradient-custom__nav" id="navbar">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="../../index.html">
+                <img className="logo__size" alt="" src={Logo}/>   
+                </a>
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse"  id="navbarToggleExternalContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="NavBar__Fromat nav-item nav__ul__border ms-1 body__header__border body__header__nav__ul__textcolor"><a class="nav-link" href="../../index.html">Inicio</a></li>
+                        <li class="NavBar__Fromat nav-item nav__ul__border ms-1 body__header__border body__header__nav__ul__textcolor"><a class="nav-link" href="about.html">Nosotros</a></li>
+                        <li class="NavBar__Fromat nav-item nav__ul__border ms-1 body__header__border body__header__nav__ul__textcolor"><a class="nav-link" href="clients.html">Nuestros Clientes</a></li>
+                        <li class="NavBar__Fromat nav-item nav__ul__border ms-1 body__header__border body__header__nav__ul__textcolor"><a class="nav-link" href="portafolio.html">Tienda</a></li>
+                        <li class="NavBar__Fromat nav-item nav__ul__border ms-1 body__header__border body__header__nav__ul__textcolor"><a class="nav-link" href="contact.html"><CartWidget/></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </div>
     )
  }
